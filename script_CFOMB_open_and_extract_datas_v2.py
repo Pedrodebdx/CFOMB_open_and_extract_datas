b5361d8d04685c58c1c02d07ffd37a8517c32a72
pierre-vincent.ferrat@exco.fr <pierre-vincent.ferrat@exco.fr>
2020-09-18 10:26:40 +0200

    add comments and some maj

--- script_CFOMB_open_and_extract_datas_v2.py
+++ script_CFOMB_open_and_extract_datas_v2.py
@@ -4,25 +4,24 @@ import os
 
 file='4.dat' #choose your cfonb file
 statement_file = open(file)
-reader = StatementReader()
+reader = StatementReader() #reader of library
 
-try:
+try: # for classics files  with carriage return
     result =reader.parse(statement_file)
-    print('dans le try')
 
-except: 
-    print('dans le except')
+except: # for files without carriage return
     statement_file = open(file).readlines()
-    statement_file = statement_file[0]
-    tmp = '\r\n'.join(statement_file[i:i+120] for i in range(0, len(statement_file), 120))
+    statement_file = statement_file[0] # firt because just a long string
+    tmp = '\r\n'.join(statement_file[i:i+120] for i in range(0, len(statement_file), 120)) # add a carriage return every 120 characters
 
     with open('tmp.txt', 'w+') as fh:
         fh.write(tmp)   
         statement_file = open('tmp.txt')
         result =reader.parse(statement_file)
-        os.remove("tmp.txt")
+        os.remove("tmp.txt") #remove temp file
         
 for j in list(range(0,len(result))):
-    print(result[j].header["account_nb"])
-    print(result[j].footer["account_nb"])
-
+    print(f'Compte numéro : {result[j].header["bank_code"]} {result[j].header["desk_code"]} {result[j].header["account_nb"]}')
+    print(f'Solde début ({result[j].header["prev_date"]}) : {result[j].header["prev_amount"]}')
+    print(f'Solde fin ({result[j].footer["next_date"]}) : {result[j].footer["next_amount"]}')
+    print(' ')
