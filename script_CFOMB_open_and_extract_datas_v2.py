0476d28f6bb657da4e157a57153151ed8321a04c
pierre-vincent.ferrat@exco.fr <pierre-vincent.ferrat@exco.fr>
2020-10-01 15:21:01 +0200

    première version front avec flask fonctionnelle

--- script_CFOMB_open_and_extract_datas_v2.py
+++ script_CFOMB_open_and_extract_datas_v2.py
@@ -1,27 +1,61 @@
 import csv
-from pythoncfonb import StatementReader
+from cfonb import StatementReader
 import os
 
-file='2.dat' #choose your cfonb file
-statement_file = open(file)
-reader = StatementReader() #reader of library
+def script(file='4.dat'): #choose your cfonb file
+    
+    statement_file = open(file)
+    reader = StatementReader() #reader of library
 
-try: # for classics files  with carriage return
-    result =reader.parse(statement_file)
-
-except: # for files without carriage return
-    statement_file = open(file).readlines()
-    statement_file = statement_file[0] # firt because just a long string
-    tmp = '\r\n'.join(statement_file[i:i+120] for i in range(0, len(statement_file), 120)) # add a carriage return every 120 characters
-
-    with open('tmp.txt', 'w+') as fh:
-        fh.write(tmp)   
-        statement_file = open('tmp.txt')
+    try: # for classics files  with carriage return
         result =reader.parse(statement_file)
-        os.remove("tmp.txt") #remove temp file
-        
-for j in list(range(0,len(result))):
-    print(f'Compte numéro : {result[j].header["bank_code"]} {result[j].header["desk_code"]} {result[j].header["account_nb"]}')
-    print(f'Solde début ({result[j].header["prev_date"]}) : {result[j].header["prev_amount"]}')
-    print(f'Solde fin ({result[j].footer["next_date"]}) : {result[j].footer["next_amount"]}')
-    print(' ')
+        print('on est dans le try')
+    except: # for files without carriage return
+        statement_file = open(file).readlines()
+        statement_file = statement_file[0] # firt because just a long string
+        tmp = '\r\n'.join(statement_file[i:i+120] for i in range(0, len(statement_file), 120)) # add a carriage return every 120 characters
+        print('on est dans le except')
+        with open('tmp.txt', 'w+') as fh:
+            fh.write(tmp)   
+            statement_file = open('tmp.txt')
+            result =reader.parse(statement_file)
+            os.remove("tmp.txt") #remove temp file
+
+    liste_sortie = []        
+    for j in list(range(0,len(result))):
+        #print(f'Compte numéro : {result[j].header["bank_code"]} {result[j].header["desk_code"]} {result[j].header["account_nb"]}')
+        #print(f'Solde début ({result[j].header["prev_date"]}) : {result[j].header["prev_amount"]}')
+        #print(f'Solde fin ({result[j].footer["next_date"]}) : {result[j].footer["next_amount"]}')
+        #print(' ')
+        ligne1 = f'Compte numéro : {result[j].header["bank_code"]} {result[j].header["desk_code"]} {result[j].header["account_nb"]}'
+        liste_sortie.append(ligne1)
+        ligne2 = f'Solde début ({result[j].header["prev_date"]}) : {result[j].header["prev_amount"]}'
+        liste_sortie.append(ligne2)
+        ligne3 = f'Solde fin ({result[j].footer["next_date"]}) : {result[j].footer["next_amount"]}'
+        liste_sortie.append(ligne3)
+        ligne4 = ' '
+        liste_sortie.append(ligne4)
+    return liste_sortie
+ 
+
+
+
+from flask import *  
+app = Flask(__name__)  
+app.config['MAX_CONTENT-PATH'] = 99999999999
+app.config['UPLOAD_FOLDER'] = 'datas'
+
+@app.route('/')  
+def upload():  
+    return render_template("fileuploadform1.html")  
+
+@app.route('/success', methods = ['POST'])  
+def success():   
+    if request.method == 'POST':  
+        f = request.files['file']  
+        f.save(f.filename)
+        liste_sortie= script(file=f.filename)
+        os.remove(f.filename)  # delate file uploaded
+    return render_template("success4.html", name = f.filename, liste_sortie=liste_sortie)  
+if __name__ == '__main__':
+    app.run(debug = True) 
