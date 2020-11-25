2d7dcb486fc776e49f4662e485fc6c17fb82353f
pierre-vincent.ferrat@exco.fr <pv.ferrat@gmail.com>
2020-11-25 16:04:38 +0100

    reorganisation du projet + travail sur export csv (fonctionnel mais erreur de dockerisation pandas)'

--- my_app/old_version/static/img_to_dataurl_wo_xml.js
+++
@@ -1,5 +0,0 @@
-function upload_img() {
-    var canvas = document.getElementById("canvas");
-    var dataURL = canvas.toDataURL("image/png");
-    document.getElementById('data').value = dataURL;
-};
